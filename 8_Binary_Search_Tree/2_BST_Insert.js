ad4c7b83a61e329e8986ac265107a61fa4baf8c5
Juan de Dios <jddios837@gmail.com>
2022-12-05 12:24:56 -0500

    Find in BST

--- 8_Binary_Search_Tree/2_BST_Insert.js
+++ 8_Binary_Search_Tree/2_BST_Insert.js
@@ -40,11 +40,69 @@ class BinarySearchTree {
                     // else {
                     //     currentNode = currentNode.right;
                     // }
-                    currentNode = currentNode.left;
+                    currentNode = currentNode.right;
                 }
             }
         }
     }
+
+    find(value) {
+        if (this.root === null) return false;
+
+        let current = this.root, found = false;
+
+        while (current && !found) {
+            if(value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                found = true;
+            }
+        }
+        if (!found) return undefined;
+        return current;
+    }
+
+    contain(value) {
+        if (this.root === null) return false;
+
+        let current = this.root, found = false;
+
+        while (current && !found) {
+            if(value < current.value) {
+                current = current.left;
+            } else if (value > current.value) {
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // find(value) {
+    //     if (this.root === null) return -1;
+    //
+    //     if (this.root.value === value) {
+    //         return this.root;
+    //     } else {
+    //         let currentNode = this.root;
+    //         while(true){
+    //             if (currentNode.value === value) return currentNode;
+    //
+    //             if (value < currentNode.value) {
+    //                 // left path
+    //                 if (currentNode.left != null) currentNode = currentNode.left
+    //                 else return -1;
+    //             } else {
+    //                 // right path
+    //                 if (currentNode.right != null) currentNode = currentNode.right;
+    //                 else return -1;
+    //             }
+    //         }
+    //     }
+    // }
 }
 
 // A Valid Tree
@@ -60,6 +118,11 @@ tree.insert(2);
 tree.insert(16);
 tree.insert(7);
 
+console.log('---------- FIND NODE')
+console.log(tree.find(100));
+console.log(tree.contain(100));
+console.log(tree.find(13));
+console.log(tree.contain(13));
 console.log('hola');
 
 
